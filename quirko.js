73f3b113f92a3bda9efedb4797dc895843247245
sanjidamahi <mahisanjida5@gmail.com>
2025-08-14 12:55:05 +0100

    Delete quirko.js

--- quirko.js
+++
@@ -1,9 +0,0 @@
-// theme-toggle.js
-
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById('theme-switch');
-
-    btn.addEventListener('click', () => {
-        document.body.classList.toggle('darkmode');
-    });
-});
